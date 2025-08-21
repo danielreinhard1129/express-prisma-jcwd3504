183f359bd3f30e4c5ed2a646ba76078951c70c69
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2025-08-21 10:24:30 +0700

    feat: add reset password (#2)

--- src/modules/auth/auth.router.ts
+++ src/modules/auth/auth.router.ts
@@ -1,18 +1,28 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
+import { JwtMiddleware } from "../../middlewares/jwt.middleware";
+import { validateResetPassword } from "../../validators/auth.validator";
 
 export class AuthRouter {
   private router: Router;
   private authController: AuthController;
+  private jwtMiddleware: JwtMiddleware;
 
   constructor() {
     this.router = Router();
     this.authController = new AuthController();
+    this.jwtMiddleware = new JwtMiddleware();
     this.initializedRoutes();
   }
 
   private initializedRoutes = () => {
     this.router.post("/forgot-password", this.authController.forgotPassword);
+    this.router.patch(
+      "/reset-password",
+      this.jwtMiddleware.verifyToken(process.env.JWT_SECRET!),
+      validateResetPassword,
+      this.authController.resetPassword
+    );
   };
 
   getRouter = () => {
